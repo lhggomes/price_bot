9bcc49419a6c15eef2ea5da5690e38bafa47f841
Lucas Henrique Gomes <lucas.henrique.s.go@gmail.com>
2021-07-06 23:18:16 -0300

    scraping.py

--- price_bot/settings.py
+++ price_bot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
