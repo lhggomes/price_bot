3128454be4ae5bfa872c641f35e0bbf16d9a4651
Lucas Henrique Gomes <lucas.henrique.s.go@gmail.com>
2021-07-07 21:48:42 -0300

    Updating

--- price_bot/settings.py
+++ price_bot/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-
+ALLOWED_HOSTS = ['pcheck-bot.herokuapp.com']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
