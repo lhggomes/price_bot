2d32c2d7b0f1435a58d930872402d90222f03431
lucas.henrique.gomes <lucas.henrique.gomes@accenture.com>
2021-07-09 10:03:49 -0300

    Testing

--- price_bot/updater.py
+++ price_bot/updater.py
@@ -5,5 +5,5 @@ from .scraping import get_product
 # 3 hours to check
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(get_product, 'interval', seconds=60)
+    scheduler.add_job(get_product, 'interval', seconds=1200)
     scheduler.start()
