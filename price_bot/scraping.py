ab1543a3c34fa8a40d195667f0ee15504ee5b3ce
Lucas Henrique Gomes <lucas.henrique.s.go@gmail.com>
2021-07-10 12:19:28 -0300

    Updating files, to match new process

--- price_bot/scraping.py
+++ price_bot/scraping.py
@@ -19,7 +19,7 @@ def create_company_and_product_list():
 
 
 def get_product():
-    print(f'Inicio Processamento de Captura de ')
+    print(f":::> Inicio do processo de captura de dados")
     companies, products = create_company_and_product_list()
     for company in companies:
         for product in products:
@@ -29,24 +29,30 @@ def get_product():
                 try:
                     req = Request(web_site, headers=HEADERS)
                     response = urlopen(req)
+                    print(f":::> RESPONSE: {response}")
+                    print(response)
+
                     html = response.read()
                     html = html.decode('utf-8')
                     html = format_html(html)
+                    print(f":::> HTML: {html}")
 
                     soup = BeautifulSoup(html, 'html.parser')
-
+                    print(f":::> SOUP: {soup}")
                     try:
                         elements = \
                             soup.findAll('div', {"class": str(company.price_div)}, limit=1)
-
+                        print(f":::> ELEMENTS: {elements}")
                         elements = elements[0] if elements else None
                         if elements is None:
                             elements = \
                                 soup.findAll('span', {"class": str(company.price_div)}, limit=1)
                             elements = elements[0] if elements else None
 
-                        value = float(elements.text.split('R$ ')[-1].replace('.', "").replace(",", ".")) if elements is not None else float(9999999)
-                        print(f'Teste Ok')
+                        value = float(elements.text.split('R$ ')[-1].replace('.', "").replace(",",
+                                                                                              ".")) if elements is not None else float(
+                            9999999)
+                        print(f":::> VALOR: {value}")
                         if value <= min_price.min_value:
                             try:
 
@@ -66,14 +72,19 @@ def get_product():
                                 REVENDEDORA: {web_site}
                                 
                                 """
-                                send_mail(
-                                    'ALERTA DE PREÇO: BOT',
-                                    email_template,
-                                    'price.bot.checker@gmail.com',
-                                    ['lucas.henrique.s.go@gmail.com', 'cleidercsa@gmail.com',
-                                     'lucas.henrique@a.unileste.edu.br'],
-                                    fail_silently=False
-                                )
+                                try:
+                                    send_mail(
+                                        'ALERTA DE PREÇO: BOT',
+                                        email_template,
+                                        'price.bot.checker@gmail.com',
+                                        ['lucas.henrique.s.go@gmail.com', 'cleidercsa@gmail.com',
+                                         'lucas.henrique@a.unileste.edu.br'],
+                                        fail_silently=False
+                                    )
+                                    print(f":::> Email enviado com sucesso!!")
+                                except Exception as e:
+                                    print(f":::> Não foi possível enviar o email: {e.__repr__()}")
+                                    raise Exception(e.__repr__())
 
                             except Exception as e:
                                 send_mail(
@@ -83,7 +94,8 @@ def get_product():
                                     ['lucas.henrique.s.go@gmail.com'],
                                     fail_silently=False
                                 )
-                                print(f'Cannot perform update for this task: {e.__repr__()}')
+                                print(f':::> Cannot perform update for this task: {e.__repr__()}')
+                                raise Exception(e.__repr__())
 
                     except Exception as e:
                         send_mail(
@@ -93,7 +105,8 @@ def get_product():
                             ['lucas.henrique.s.go@gmail.com', 'lucas.henrique@a.unileste.edu.br'],
                             fail_silently=False
                         )
-                        print(e.__repr__())
+                        print(f":::> {e.__repr__()}")
+                        raise Exception(e.__repr__())
 
                 except HTTPError as e:
                     send_mail(
@@ -103,4 +116,4 @@ def get_product():
                         ['lucas.henrique.s.go@gmail.com', 'lucas.henrique@a.unileste.edu.br'],
                         fail_silently=False
                     )
-                    print(e.status, e.reason)
+                    print(f":::> Não foi possível realizar parse {e.status} -  {e.reason}")
