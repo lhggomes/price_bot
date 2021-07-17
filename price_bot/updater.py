cf2500da4bd559e1a565857553c9a2f902e03400
Lucas Henrique Gomes <lucas.henrique.s.go@gmail.com>
2021-07-17 13:01:49 -0300

    Updating files, to match new process

--- price_bot/updater.py
+++ price_bot/updater.py
@@ -5,5 +5,5 @@ from .scraping import get_product
 # 3 hours to check
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(get_product, 'interval', seconds=600)
+    scheduler.add_job(get_product, 'interval', seconds=120)
     scheduler.start()
